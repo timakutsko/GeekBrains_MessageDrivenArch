a96f35db5e0a8a9e0a057c7cde7dbb3039066666
Tsimafei <tima.kutsko@gmail.com>
2023-04-02 18:51:40 +0300

    Lesson2. AMQP (с помощью RabbitMQ)

---
+++ RestServiceL2/Restaurant.Booking/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Restaurant.Booking
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Restaurant restaurant = new Restaurant();
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            while (true)
+            {
+                Console.WriteLine(">>>Привет! Бронируем столик? ");
+                
+                await Task.Delay(5000);
+
+                Stopwatch stopWatchUnBook = new Stopwatch();
+                stopWatchUnBook.Start();
+                restaurant.BookFreeTableAsync(1);
+                stopWatchUnBook.Stop();
+                
+                TimeSpan tsub = stopWatchUnBook.Elapsed;
+                Console.WriteLine($">>>Время на обработку: {tsub.Seconds:00}:{tsub.Milliseconds:000}");
+            }
+        }
+    }
+}
